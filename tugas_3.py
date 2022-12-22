78887c62f097b7e931695013b77c520a00a81d64
Khoerunisa Salma Rahmadhani <115153134+NisaSalma@users.noreply.github.com>
2022-12-22 14:09:46 +0700

    Add files via upload

---
+++ tugas_3.py
@@ -0,0 +1,7 @@
+def tampil_kesukaan(kesukaan1,kesukaan2,kesukaan3,kesukaan4,kesukaan5):
+    print("kesukaanku adalah"' ' + kesukaan2)
+
+tampil_kesukaan(kesukaan1 = "membaca", kesukaan2 = "mempelajari hal-hal baru",
+ kesukaan3 = "mendengarkan musik", kesukaan4 = "meyendiri", kesukaan5 = "scroll toktok" )
+ #outputnya ialah kesukaanku adalah mempelajari hal-hal baru
+
